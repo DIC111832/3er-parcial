b7db3df57a28cc94275213e91e311fc161824595
DIC111832 <168487362+DIC111832@users.noreply.github.com>
2024-05-23 01:22:07 -0600

    Add files via upload

---
+++ 4_suma_de_tres_numeros_distintos.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b,c,suma;
+    cout << "ingrese los valores que desee a tres variables";
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    suma = a+b+c;
+    cout << " la suma de los numeros  ";
+    cout << "\n" << a;
+    cout << "\n" << b;
+    cout << "\n" << c;
+    cout << "\n es de : " << suma;
+    return 0;
+}
