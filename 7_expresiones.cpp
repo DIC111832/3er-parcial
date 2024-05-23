79a6f27adea31a649bbbfe3b0ece374c2bb9afcb
DIC111832 <168487362+DIC111832@users.noreply.github.com>
2024-05-23 10:56:20 -0600

    Add files via upload

---
+++ 7_expresiones.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int i1,i2;
+    float r1,r2;
+    float r3,r4,r5;
+
+    i1=12;
+    i2=5;
+    r1=12.0;
+    r2=5.0;
+
+    r3=r1/r2;
+    r4=i1/i2;
+    r5=i1/r2;
+
+    cout << "\n" << r3;
+    cout << "\n" << r4;
+    cout << "\n" << r5;
+
+    return 0 ;
+
+
+
+}
