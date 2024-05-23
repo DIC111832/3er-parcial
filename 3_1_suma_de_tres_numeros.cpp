9f16fa893ef7ea6561c9989d7f89e5574151109a
DIC111832 <168487362+DIC111832@users.noreply.github.com>
2024-05-23 01:04:29 -0600

    Add files via upload

---
+++ 3_1_suma_de_tres_numeros.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,b,c;
+    int suma;
+    a=5;
+    b=7;
+    c=9;
+
+    suma = a+b+c;
+    cout << "la suma de 5, 7, 9 es de : " << suma;
+    return 0;
+}
