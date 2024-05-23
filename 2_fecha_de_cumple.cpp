337207b71c67c455ade2238ddb440010127cfe82
DIC111832 <168487362+DIC111832@users.noreply.github.com>
2024-05-23 00:57:41 -0600

    Add files via upload

---
+++ 2_fecha_de_cumple.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int a,c;
+    cout << "introduzca su año de nacimiento";
+    cin >>   a;
+    cout << " si usted nacio en el año " <<   a;
+    c = 2024-a;
+    cout << " \n este año usted cumple" <<   c;
+    return 0;
+
+
+}
