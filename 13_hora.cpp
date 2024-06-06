5ea901dea4f5baa6cd040f364513eb801e474b3d
DIC111832 <168487362+DIC111832@users.noreply.github.com>
2024-06-05 22:18:52 -0600

    13_hora.cpp

--- 13_hora.cpp
+++ 13_hora.cpp
@@ -4,13 +4,22 @@ int main (){
     int hora;
     cout << " hola, que hora es?";
     cin >> hora;
-    if(hora>12)
+    if (hora>=20 && hora < 24)
+    {
+        cout << "buenas noches";
+    }
+    if(hora>12 && hora<19)
     {
         cout << "buenas tardes";
     }
-    if (hora<12)
+    if (hora<12 && hora>1)
     {
         cout << "buenos dias";
     }
+    if (hora<1 || hora >25)
+    {
+        cout << "numero no valido, ingresar otro numero";
+    }
+    
     return 0;
 }
